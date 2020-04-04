528cf3b8055f4a2bf7cd4d2f11434681fb7f1d1c
0N3IR0S <8enibue@gmail.com>
2020-04-04 19:33:59 +0200

    create test sorting algorithm, working with time.sleep

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # 2020
 
 import pygame
-from random import randint
+import random
+import time
 
 # initialize pyGame
 pygame.init()
 
 # create screen
-game_screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption("sortingAlgorithm by @0N3IR0S")
@@ -19,17 +20,38 @@ clock = pygame.time.Clock()
 
 # main loop
 game_exit = False
+run_once = False
 while not game_exit:
     for event in pygame.event.get():
         # Quit Program
         if event.type == pygame.QUIT:
             game_exit = True
 
-    # game screen
-    game_screen.fill((0, 0, 0))
+    # run this code only once
+    while not run_once:
+        run_once = True
 
-    # create random array
-    
+        # game screen
+        screen.fill((0, 0, 0))
+
+        # create random array
+        random_list = [random.randrange(5, 400, 5) for i in range(86)]
+
+        # sort this shit
+        for i in random_list:
+            k = 85
+            for j in range(0, k):
+                if random_list[j + 1] > random_list[j]:
+                    random_list[j], random_list[j + 1] = random_list[j + 1], random_list[j]
+                k -= 1
+
+                screen.fill((0, 0, 0))
+
+                xx = 0
+                for l in range(99, 701, 7):
+                    pygame.draw.rect(screen, (255, 255, 255), [(800 - l), 500, 5, (random_list[xx] * -1)])
+                    xx += 1
+            pygame.display.update()
 
     # update display
     pygame.display.update()
